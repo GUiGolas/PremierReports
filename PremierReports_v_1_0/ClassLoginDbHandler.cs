b527ecd8fe0ba237314d20e2d92a60f6f2fe24cd
Guigolas <gui2519@gmail.com>
2015-04-27 16:18:32 -0300

    added ClassLoginDbHandler

---
+++ PremierReports_v_1_0/ClassLoginDbHandler.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+/// <summary>
+/// Summary description for ClassLoginDbHandler
+/// </summary>
+public class ClassLoginDbHandler
+{
+    public string userName { get; set; }
+    public string hash { get; set; }
+    public bool root { get; set; }
+
+    
+    public ClassLoginDbHandler()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+
+    }
+
+    public bool CheckUser(string _userName, string _hash)
+    {
+        DataTable dt;
+        string conection = @"Data Source=(LocalDB)\v11.0;AttachDbFilename=\\servidor\particulares\GUILHERME\Visual Studio 2013\WebSites\InterfaceTest2\App_Data\ReportsDB.mdf;Integrated Security=True";
+
+        using (SqlConnection con = new SqlConnection(conection))
+        {
+            using (SqlCommand cmd = con.CreateCommand())
+            {
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = "select Password from Users where username = @uname";
+                cmd.Parameters.Add(new SqlParameter("@uname", _userName));
+
+                using (SqlDataAdapter da = new SqlDataAdapter(cmd))
+                {
+                    dt = new DataTable();
+                    da.Fill(dt);
+                    if (dt.Rows.Count < 1) return false;
+                }
+            }
+        }
+
+        md5 encript = new md5(_hash.Trim());
+        encript.Encript();
+
+        if (encript.hash == dt.Rows[0]["password"].ToString().Trim())
+        {
+            userName = _userName;
+            hash = _hash;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
+    }
+
+    public bool ChangePassword(string _userName, string _oldHash, string _newHash)
+    {
+        if (!CheckUser(_userName, _oldHash)) return false;
+        else
+        {
+
+            using (SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["userDbConnectionString"].ConnectionString))
+            {
+                using (SqlCommand cmd = con.CreateCommand())
+                {
+                    cmd.CommandType = CommandType.Text;
+                    cmd.CommandText = "UPDATE Table SET Password = @password where username = @uname";
+                    cmd.Parameters.Add(new SqlParameter("@uname", _userName));
+                    cmd.Parameters.Add(new SqlParameter("@password", _newHash));
+
+                    using (SqlDataAdapter da = new SqlDataAdapter(cmd))
+                    {
+                        DataTable dt = new DataTable();
+                        if (da.Fill(dt) > 0)
+                        {
+                            return true;
+                        }
+                        else
+                        {
+                            return false;
+                        }
+
+                    }
+                }
+            }
+
+
+
+
+        }
+
+
+    }
+}
--- PremierReports_v_1_0/index.aspx.cs
+++ PremierReports_v_1_0/index.aspx.cs
@@ -4,14 +4,76 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Globalization;
+using System.Threading;
+using System.Web.Security;
 
 namespace PremierReports_v_1_0
 {
     public partial class index : System.Web.UI.Page
     {
+        private Int16 langNumber = 0;
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (CultureInfo.CurrentCulture.DisplayName.ToString() != "Português (Brasil)")
+            {
+                langNumber = 1;
+            }
+            else
+            {
+                langNumber = 0;
+            }
+        }
 
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            //   *** Validate fields *** 
+            try
+            {
+                // Validade userName field
+                if (txtUserName.Text.Trim().Length < 3)
+                {
+                    throw new Exception(ErrorConstants.getErrorMessage("userNameMinLength", langNumber));
+                }
+                // Validade the password field
+                if (txtUserName.Text.Trim().Length < 4)
+                {
+                    throw new Exception(ErrorConstants.getErrorMessage("passwordMinLength", langNumber));
+                }
+
+                // Verify if the login is valid
+                ClassLoginDbHandler loginHandler = new ClassLoginDbHandler();
+                if (!loginHandler.CheckUser(txtUserName.Text.Trim(), txtPassword.Text.Trim())) throw new Exception(ErrorConstants.getErrorMessage("loginFailed",langNumber));
+
+                //save logged user in application Session
+                Session["user"] = loginHandler.userName;
+                Session["password"] = loginHandler.hash;
+                Session["logged"] = true;
+
+                //Let us now set the authentication cookie so that we can use that later.
+                FormsAuthentication.SetAuthCookie(loginHandler.userName, false);
+
+                //Login successful lets put him to requested page
+                string returnUrl = Request.QueryString["ReturnUrl"] as string;
+
+                if (returnUrl != null)
+                {
+                    Response.Redirect(returnUrl);
+                }
+                else
+                {
+                    //no return URL specified so lets kick him to home page
+                    Response.Redirect("Login.aspx");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                lblLoginError.Text = ex.Message;
+                lblLoginError.Visible = true;
+                //throw;
+            }
         }
     }
 }
